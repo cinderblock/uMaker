749320a0145c943e0d3ec7c7454085cd0acb6b99
Cameron Tacklind <cameron@tacklind.com>
2014-06-21 01:06:45 -0700

    Updating NRF51 namespace

--- tools/nRFinit.mk
+++ tools/nRFinit.mk
@@ -1,21 +1,24 @@
 # This Makefile, unlike most others, is reaally only intended to be used once.
 # It will copy the necessary template files from the base nRF folder.
 
+ifeq $(VARS_INCLUDE),nRF51
+ $(error You need to include vars/nRF51.mk \(or equivalent\) to use this uMaker tool)
+endif
 
-NRF_INIT_FILES_BASE ?= system_nrf51422.c gcc/gcc_startup_nrf51.s gcc/gcc_nrf51_common.ld gcc/gcc_nrf51_s110_xxaa.ld
+NRF51_INIT_FILES_BASE ?= system_nrf51422.c gcc/gcc_startup_nrf51.s gcc/gcc_nrf51_common.ld gcc/gcc_nrf51_s110_xxaa.ld
 
-NRF_INIT_FILES ?= $(NRF_INIT_FILES:%=$(NRF_INITDIR)%)
+NRF51_INIT_FILES ?= $(NRF51_INIT_FILES:%=$(NRF51_INITDIR)%)
 
 ##### Targets
 
-nRFinit: $(NRF_INIT_FILES)
+nRFinit: $(NRF51_INIT_FILES)
 
 $(NRF_INITDIR)%:
 	$(ECO) "cp nRF	$@"
-	cp $(@:$(NRF_INITDIR)=$(NRF_TEMPLATE_DIR)) $@
+	cp $(@:$(NRF51_INITDIR)=$(NRF51_TEMPLATE_DIR)) $@
 
-.SECONDARY: $(NRF_INIT_FILES)
+.SECONDARY: $(NRF51_INIT_FILES)
 
 # Add directory targets to those that need them
 .SECONDEXPANSION:
-$(NRF_INIT_FILES): | $$(dir $$@)
+$(NRF51_INIT_FILES): | $$(dir $$@)
