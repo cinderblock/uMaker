86bdd9df7ce0aff17842150b532a70153136155c
Cameron Tacklind <cameron@tacklind.com>
2015-02-23 02:06:13 -0800

    New better variables for automatic assembly file generation

--- tools/assembly.mk
+++ tools/assembly.mk
@@ -1,12 +1,21 @@
 # Create assembly (.s/.S) files from C/C++ sources
 
+ASM ?= $(C:%=%.c.s) $(CPP:%=%.cpp.S)
+
+ASM_DIR ?= $(BLD_DIR)
+
+ASM_FILES ?= $(ASM:%=$(ASM_DIR)%)
+
+asm: $(ASM_FILES)
 
 # Create assembly files from .c sources
-$(BLD_DIR)%.c.s: $(SRCDIR)%.c
+$(ASM_DIR)%.c.s: $(SRCDIR)%.c $(MAKEFILE_LIST)
 	$(ECO) "CC AS	$@"
 	$(BLD_GCC) -o $@ $< -S $(BLD_GCCFLAGS_FINAL)
 
-	# Create object files from .cpp sources
-$(BLD_DIR)%.cpp.S: $(SRCDIR)%.cpp
+# Create object files from .cpp sources
+$(ASM_DIR)%.cpp.S: $(SRCDIR)%.cpp $(MAKEFILE_LIST)
 	$(ECO) "C++ AS	$@"
 	$(BLD_GXX) -o $@ $< -S $(BLD_GXXFLAGS_FINAL)
+
+AUTO_GENERATED_FILES += $(ASM_FILES)
