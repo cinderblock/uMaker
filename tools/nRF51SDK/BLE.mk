a2221f6a233e8ca3ac1d31259d06ca4b23b93f03
Cameron Tacklind <cameron@tacklind.com>
2015-03-01 19:19:34 -0800

    New SDK support nearly compiles fully. Missing linker scripts.
    Created new nRF51 specific example Makefile (and renamed old sample to AVR)
    
    Moving nRF includes to own file since they seem to just be used everywhere...
    
    The BLE component compiles fully, with the exception of some files that are either broken or need specific soft devices.

--- tools/nRF51SDK/BLE.mk
+++ tools/nRF51SDK/BLE.mk
@@ -11,11 +11,23 @@ NRF51_BLE_SRCDIR ?= $(NRF51_SRCDIR)$(NRF51_BLE)/
 NRF51_BLE_BLDDIR ?= $(BLD_DIR)nRF51/$(NRF51_BLE)/
 
 # Names of nRF51 source files to "find" and include
-NRF51_BLE_C ?= ble_advdata
+NRF51_BLE_C ?= *
 
 NRF51_BLE_SRC_FILES ?= $(NRF51_BLE_C:%=%.c)
 
-NRF51_BLE_SRC_FILES_FULL ?= $(foreach file,$(NRF51_BLE_SRC_FILES),$(shell find $(NRF51_BLE_SRCDIR) -type f -name $(file)))
+# BUGFIX: Broken nRF51 sources
+NRF51_BLE_SRC_FILES_BROKEN ?= %ble_services/ble_sps/ble_sps.c
+
+# TODO: Handle this better
+# If the user isn't using a multi-device soft device, don't build device_manager_central.c
+ifeq ($(NRF51_SOFTDEVICE_VERSION),s110)
+ NRF51_BLE_SRC_FILES_AUTO_FILTER += %ble/device_manager/device_manager_central.c
+endif
+
+# Filter broken sources and others
+NRF51_BLE_SRC_FILES_FILTER ?= $(NRF51_BLE_SRC_FILES_BROKEN) $(NRF51_BLE_SRC_FILES_AUTO_FILTER)
+
+NRF51_BLE_SRC_FILES_FULL ?= $(filter-out $(NRF51_BLE_SRC_FILES_FILTER),$(foreach file,$(NRF51_BLE_SRC_FILES),$(shell find $(NRF51_BLE_SRCDIR) -type f -name $(file))))
 
 NRF51_BLE_AR ?= nRF51-$(NRF51_BLE).a
 
@@ -25,15 +37,25 @@ NRF51_BLE_OUT ?= $(BLD_LIBDIR)$(NRF51_BLE_AR)
 
 NRF51_BLE_GCCFLAGS_FINAL ?= $(BLD_GCCFLAGS_FINAL) $(NRF51_BLE_GCCFLAGS_EXTRA)
 
-NRF51_BLE_INCLUDES ?= $(sort $(dir $(NRF51_BLE_SRC_FILES_FULL)))
-
-AUTO_INC += $(NRF51_BLE_INCLUDES)
-
 AUTO_LIB += $(NRF51_BLE_OUT)
 
 NRF51_BLE_TARGET ?= nRF51/BLE
 
 AUTO_GENERATED_FILES += $(NRF51_BLE_OUT) $(NRF51_BLE_OBJS) $(NRF51_BLE_DEPFILES)
+
+# Nordic's ble_cgm.c references constants that have been removed
+ifneq (,$(findstring ble_cgm.c,$(NRF51_BLE_SRC_FILES_FULL)))
+ NRF51_BLE_MISSING_DEFS_AUTO += CGM_MEASUREMENT_CHAR=0x2A6C
+ NRF51_BLE_MISSING_DEFS_AUTO += CGM_FEATURES_CHAR=0x2A6D
+ NRF51_BLE_MISSING_DEFS_AUTO += CGM_STATUS_CHAR=0x2A6E
+ NRF51_BLE_MISSING_DEFS_AUTO += CGM_SESSION_START_TIME_CHAR=0x2A6F
+ NRF51_BLE_MISSING_DEFS_AUTO += CGM_APPLICATION_SECURITY_POINT_CHAR=0x2A70
+ NRF51_BLE_MISSING_DEFS_AUTO += CGM_SPECIFIC_OPS_CONTROL_POINT_CHAR=0x2A71
+ NRF51_BLE_MISSING_DEFS_AUTO += BLE_UUID_CGM_SERVICE=0x181A
+endif
+
+AUTO_DEF += $(NRF51_BLE_MISSING_DEFS_AUTO)
+
 
 ##### Targets
 
