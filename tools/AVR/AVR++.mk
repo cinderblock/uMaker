fb1e1da296348329c3b2f33878c95bc761332fb5
Cameron Tacklind <cameron@tacklind.com>
2015-03-13 13:12:29 -0700

    Adding AVR++ build script

---
+++ tools/AVR/AVR++.mk
@@ -0,0 +1,45 @@
+
+# Location of AVR++ library. There should be a folder 'AVR++' inside this one
+AVRpp_BASEDIR ?= AVR++/
+
+# base source names to build
+AVRpp_SRC ?= ADC USART DecPrintFormatter
+
+# Relative to AVRpp_BASEDIR
+AVRpp_FILES ?= $(AVRpp_SRC:%=AVR++/%.cpp)
+
+AVRpp_AR ?= AVR++.a
+
+AVRpp_OBJS ?= $(AVRpp_FILES:%=$(BLD_DIR)%.o)
+
+AVRpp_OUT ?= $(BLD_LIBDIR)$(AVRpp_AR)
+
+AUTO_LIB += $(AVRpp_OUT)
+
+AVRpp_TARGET ?= AVR++
+
+AUTO_INC += $(AVRpp_BASEDIR)
+
+##### Targets
+
+$(BLD_DIR)%.cpp.o: $(AVRpp_BASEDIR)%.cpp
+	$(ECO) "AVR++		$@"
+	$(BLD_GXX) $< -o $@ -c $(BLD_GXXFLAGS_FINAL)
+
+$(AVRpp_OUT): $(AVRpp_OBJS)
+	$(ECO) "AVR++ AR	$@"
+	$(BLD_ARR) $@ $(AVRpp_OBJS)
+
+$(AVRpp_OUT) $(AVRpp_OBJS) : $(MAKEFILE_LIST)
+
+$(AVRpp_TARGET): $(AVRpp_OUT)
+
+.PHONY: $(AVRpp_TARGET)
+.PRECIOUS: $(AVRpp_OBJS)
+.SECONDARY: $(AVRpp_OUT)
+
+# Explicitly include all our build dep files
+AVRpp_DEPFILES ?= $(AVRpp_OBJS:$(BLD_DIR)%=$(BLD_DEPDIR)%.d)
+-include $(AVRpp_DEPFILES)
+
+AUTO_GENERATED_FILES += $(AVRpp_OUT) $(AVRpp_OBJS) $(AVRpp_DEPFILES)
